d94a4e0fa6a56a11d7fdf9b4a41e2920a9aabcb0
Matej Mihalik <matej.mihalik@capco.com>
2018-04-30 10:11:53 +0200

    Load course information via ajax

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,32 @@
 $(function() {
+  let badges = $('#badges');
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Matej_Mihalik.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(course => {
+        let badge = $('<div>', {
+          class: 'course'
+        });
 
+        let title = $('<h3>', {
+          text: course.title
+        });
+
+        let image = $('<img>', {
+          src: course.badge
+        });
+
+        let link = $('<a>', {
+          href: course.url,
+          target: '_blank',
+          class: 'btn btn-primary',
+          text: 'See Course'
+        });
+
+        badge.append(title, image, link).appendTo(badges);
+      });
+    }
+  });
 });
